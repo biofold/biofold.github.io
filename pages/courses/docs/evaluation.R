4d0555755eeba5ca6d891385a6a6540d5e571ae2
Emidio Capriotti <emidio.capriotti@gmail.com>
2021-05-04 13:55:46 +0200

    update site

--- pages/courses/docs/evaluation.R
+++ pages/courses/docs/evaluation.R
@@ -20,7 +20,7 @@ std[i]<-sd(evaluations[,i][!is.na(evaluations[,i])])
 # Make BarPlot
 nice <- brewer.pal(3, "Pastel2")
 par(mfrow = c(1, 2), mgp=c(2,0.75,0))
-bp <- barplot(vector,  names=c(1:17),  ylim=c(1,4), xlab="Question", ylab="Mean (score)", col=nice[3], las=1, axes=FALSE, xpd=FALSE)
+bp <- barplot(vector,  names=c(1:n),  ylim=c(1,4), xlab="Question", ylab="Mean (score)", col=nice[3], las=1, axes=FALSE, xpd=FALSE)
 axis(side = 1, at = bp, labels = FALSE)
 axis(side = 2, at=seq(1,4,1), las=1) 
 
@@ -34,7 +34,7 @@ arrows(x0 = mids[i], y0 = vector[i] - std[i], x1 = mids[i], y1 = vector[i] + std
 
 # Make BoxPlot
 bop<-boxplot(evaluations, col=nice[1], ylim=c(1,4), xlab="Question", ylab="score", names=c(1:n), las=1, axes=FALSE, xpd=FALSE)
-axis(side = 1, at = seq(1,17,1) )
+axis(side = 1, at = seq(1,n,1) )
 axis(side = 2 ,at=seq(1,4,1), las=1)
 box()
 dev.off()
